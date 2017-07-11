5b317d32dca24bc1460c0f28f1fc656a446fa9e3
Brett Fuller <bfuller123@gmail.com>
2017-07-11 17:02:36 -0500

    added login functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,68 @@
 var auth = firebase.auth();
 
 var authenticate = {
-  signUp = function() {
-    var email = $('#modal-email').val().trim();
-    var password = $('#modal-password').val().trim();
+  signUp: function(email, password) {
     auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
+      $('#sign-up-modal-error').text(errorMessage);
     });
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        authenticate.clearInputs();
+      }
+    })
   },
-  signIn = function() {
-    var email = $('.modal-email').val().trim();
-    var password = $('.modal-password').val().trim();
+  signIn: function() {
+    var email = $('#sign-in-modal-email').val().trim();
+    var password = $('#sign-in-modal-password').val().trim();
     auth.signInWithEmailAndPassword(email, password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
+      $('#sign-in-modal-error').text(errorMessage);
     });
+    firebase.auth().onAuthStateChanged(function(user){
+      if (user) {
+        authenticate.clearInputs();
+      }
+    })
   },
-  signOut = function() {
+  signOut: function() {
     auth.signOut().then(function() {
       console.log('Successful sign out');
     }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
     });
+  },
+  passwordsMatch: function() {
+    var email = $('#sign-up-modal-email').val().trim();
+    var password1 = $('#sign-up-modal-password').val().trim();
+    var password2 = $('#sign-up-modal-password-2').val().trim();
+    if (password1 === password2) {
+      authenticate.signUp(email, password1);
+    }
+    else {
+      $('#sign-up-modal-error').text("Passwords do not match!");
+    }
+  },
+  clearInputs: function() {
+    $('input').val("");
+    $('#sign-up-modal-error').text("");
+    $('#sign-in-modal-error').text("");
   }
 }
 
-$('#sign-in-button').on('click', );
+$('#modal-sign-up').on('click', authenticate.passwordsMatch);
+$('#modal-sign-in').on('click', authenticate.signIn);
+$('.sign-out-button').on('click', authenticate.signOut);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    $('.login-buttons').css('visibility', 'hidden');
+    $('.sign-out-button').css('visibility', 'visible');
+  } else {
+    $('.login-buttons').css('visibility', 'visible');
+    $('.sign-out-button').css('visibility', 'hidden');
+  }
+});
